18c96d9f0ef08f4c185741342fea4a41a56f3ad8
Ritchel M <ritchiemesidor84@gmail.com>
2025-09-07 09:43:05 -0400

    FEAT: add drive train subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveTrain.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class DriveTrain {
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.rowanmcalpin.nextftc.core.Subsystem;
+import com.rowanmcalpin.nextftc.ftc.OpModeData;
+
+public class DriveTrain extends Subsystem {
+    public static final DriveTrain INSTANCE = new DriveTrain();
+    public DcMotorEx lf;
+    public DcMotorEx rf;
+    public DcMotorEx lb;
+
+    public DcMotorEx rb;
+
+
+
+    public DriveTrain(){
+
+    }
+    public void initialize(){
+        lf = OpModeData.INSTANCE.getHardwareMap().get(DcMotorEx.class, "lf");
+        rf = OpModeData.INSTANCE.getHardwareMap().get(DcMotorEx.class, "rf");
+        lb = OpModeData.INSTANCE.getHardwareMap().get(DcMotorEx.class, "lb");
+        rb = OpModeData.INSTANCE.getHardwareMap().get(DcMotorEx.class, "rb");
+    }
 }
