63571a61e8cbefcd7ef6c12bd867a57550dee174
Ritchel M <ritchiemesidor84@gmail.com>
2025-12-13 05:53:05 -0500

    pidf controller tuning

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDControls/TunePidf.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.PIDControls;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Hardware;
+
+@Config
+@TeleOp(name = "Velocity PID tuner")
+public class TunePidf extends OpMode {
+    Hardware robot = Hardware.getInstance();
+    private Telemetry telemetryA;
+    public static double kP, kI, kD, kf, iZone;
+    public static double targetRpm;
+    double targetTps;
+    FtcDashboard dashboard = FtcDashboard.getInstance();
+    PIDFController pidf;
+
+    double velError;
+    double currentVel;
+    double velPower;
+
+    public void init() {
+        telemetryA = new MultipleTelemetry(this.telemetry, dashboard.getTelemetry());
+        robot.shooter.bottomShooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.shooter.topShooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        pidf = new PIDFController(kP, kI, kD, kf, iZone);
+        //targetTps = robot.shooter.RPMtoTPS((int) targetRpm);
+
+        robot.init(hardwareMap, telemetryA);
+    }
+
+    public void loop() {
+        currentVel = Math.abs(robot.shooter.getVelocity());
+        velError = targetRpm - currentVel;
+        velPower = pidf.calculate(currentVel, targetRpm);
+
+        robot.shooter.setPower(velPower);
+        telemetryA.addData("Current Error: ", velError);
+        telemetryA.addData("Current Velocity (rpm)", currentVel);
+        telemetryA.addData("Calculated PID: ", velPower);
+        telemetryA.addData("Target Velocity", targetRpm);
+        telemetryA.update();
+    }
+}
