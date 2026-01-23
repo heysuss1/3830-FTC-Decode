d720e6bde823494e051fc8a582bcf90a15af5021
Ritchel M <ritchiemesidor84@gmail.com>
2026-01-23 10:20:33 -0500

    Implement CmdFarZoneAuto skeleton with state machine
    Added the AutoState enum and implemented the AutoCommands interface in CmdFarZoneAuto. Stub methods for buildPaths, autonomousUpdate, cancel, and getAutoState were added to prepare for future autonomous logic.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/CmdFarZoneAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/CmdFarZoneAuto.java
@@ -1,4 +1,34 @@
 package org.firstinspires.ftc.teamcode.autos;
 
-public class CmdFarZoneAuto {
+public class CmdFarZoneAuto implements AutoCommands{
+
+    enum AutoState {
+        START,
+        DRIVE_TO_SHOOTING_SPOT,
+        SHOOTING,
+        DRIVE_TO_GROUP,
+        SLURPING_GROUP,
+        DRIVE_TO_PARK, // <-- dunno if we need this but just in case
+        DRIVE_TO_GATE, // <--- these are for later when we get more balls than the pre-placed ones
+        SLURPING_FROM_GATE,
+        STOP
+    }
+
+    AutoState state = AutoState.START;
+    @Override
+    public void buildPaths(){
+
+    }
+    @Override
+    public void autonomousUpdate(){
+
+    }
+    @Override
+    public void cancel(){
+
+    }
+    @Override
+    public String getAutoState(){
+        return state.toString();
+    }
 }
